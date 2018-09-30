03f02d6f53b92605d219812dc3d46412a436c52a
roger <roger@zapatilla>
2018-09-30 17:30:08 +0200

    Administrador con comentarios incorporados

--- AccesoDatos_SErializacion/src/serializacion/Administrador.java
+++ AccesoDatos_SErializacion/src/serializacion/Administrador.java
@@ -1,26 +1,32 @@
 package serializacion;
 
 public class Administrador extends Empleado {
+	
+	//Atributos
 	private double incentivo;
 	
+	//constructores
 	public Administrador(String n,double s,int agno, int mes, int dia) {
 		
 		super(n,s,agno,mes,dia);
 		incentivo = 0;
 	}
 	
+	//getters
 	@Override
 	public double getSueldo() {
 		double sueldoBase = super.getSueldo();
 		return sueldoBase + incentivo;
 	}
 	
+	//toString
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
 		return super.toString() + " Incentivo= " + incentivo;
 	}
 	
+	//setters
 	public void setIncentivo(double incentivo) {
 		this.incentivo = incentivo;
 	}
